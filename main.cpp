c2ec2b534b9ac6aadc110ef08d7753a35171cb21
TEMU3000 <b03902051@csie.ntu.edu.tw>
2017-01-04 16:04:07 +0800

    2p, gameover

--- main.cpp
+++ main.cpp
@@ -21,11 +21,15 @@ int wide = 480;
 Mat detectAndMask( Mat frame );
 void overlayImage(const Mat &background, const Mat &foreground, Mat &output, Point2i location);
 
+Mat mask;
+Mat ship;
+
 int main(int argc, char *argv[])
 {
     Mat bg = imread("bg.png", IMREAD_UNCHANGED);
     Mat met = imread("meteor.png", IMREAD_UNCHANGED);
-    Mat ship = imread("ship.jpg", IMREAD_UNCHANGED);
+    ship = imread("ship.png",IMREAD_UNCHANGED);
+    mask = imread("mask.jpg",IMREAD_GRAYSCALE);
 
     namedWindow( "Scene", CV_WINDOW_AUTOSIZE );
 
@@ -40,6 +44,9 @@ int main(int argc, char *argv[])
     bool has_met = false;
     int score = 0;
     srand(time(NULL));
+    int player2_x = 250;
+    bool player2_alive = true;
+    Mat player2 = imread("player2.png", IMREAD_UNCHANGED);
 
     while ( capture.read(frame) ) {
         if( frame.empty() ) {
@@ -51,8 +58,20 @@ int main(int argc, char *argv[])
 
         Mat scene = bg(Rect(0,bgy-720-1,frame.cols,720)); // x,y,width,height
         Mat new_scene;
+
+        if(player2_alive){
+            player2_x = player2_x + rand()%81 - 40;
+            if(player2_x < 0){
+                player2_x = 0;
+            }
+            if(player2_x > frame.cols - player2.cols){
+                player2_x = frame.cols - player2.cols;
+            }
+            overlayImage(scene, player2, new_scene,Point(player2_x, 740 - player2.rows));
+        }
+
         int player_x_screen = player_x * frame.cols / wide - player.cols/2;
-        overlayImage(scene, player, new_scene,Point(player_x_screen, 740 - player.rows));
+        overlayImage((player2_alive) ? new_scene : scene, player, new_scene,Point(player_x_screen, 740 - player.rows));
 
         if(!has_met){
             //srand(time(NULL));
@@ -61,9 +80,38 @@ int main(int argc, char *argv[])
             overlayImage(new_scene, met, new_scene,Point(metx,-108));
             has_met = true;
         } else {
-            if(mety >= 400){
+            if(mety >= 400 && mety < 660){
                 if(abs((player_x_screen + player.cols/2) - (metx + 60) ) < 110){
+                    Mat exp1 = imread("explosion.png", IMREAD_UNCHANGED);
+                    Mat exp2 = imread("explosion2.png", IMREAD_UNCHANGED);
+                    int exp_x = (player_x_screen + metx) / 2;
+                    int exp_y = mety;
+                    Mat ex_scene;
+                    overlayImage(new_scene, exp1, ex_scene,Point(exp_x,exp_y));
+                    imshow("Scene", ex_scene);
+                    waitKey(300);
+                    overlayImage(new_scene, exp2, ex_scene,Point(exp_x,exp_y));
+                    imshow("Scene", ex_scene);
+                    waitKey(300);
+                    overlayImage(new_scene, exp1, ex_scene,Point(exp_x,exp_y));
+                    imshow("Scene", ex_scene);
+                    waitKey(300);
+                    overlayImage(new_scene, exp2, ex_scene,Point(exp_x,exp_y));
+                    imshow("Scene", ex_scene);
+                    waitKey(300);
+                    overlayImage(new_scene, exp1, ex_scene,Point(exp_x,exp_y));
+                    imshow("Scene", ex_scene);
+                    waitKey(300);
+                    Mat gameover = frame;
+                    string text = "Final Score: " + to_string(score);
+                    putText(gameover, text, Point(200,frame.rows/2), FONT_HERSHEY_DUPLEX , 1, Scalar(0,0,255));
+
+                    imshow("Scene", gameover);
+                    waitKey(5000);
                     return score;
+                }
+                if(abs((player2_x + player2.cols/2) - (metx + 60) ) < 110){
+                    player2_alive = false;
                 }
             }
             if(mety >= 720){
@@ -95,7 +143,9 @@ Mat detectAndMask( Mat frame )
 {
     std::vector<Rect> faces;
     Mat frame_gray;
-    Mat ship = imread("ship.png",IMREAD_UNCHANGED);
+    //Mat ship = imread("ship.png",IMREAD_UNCHANGED);
+    Mat ship_copy;
+    ship.copyTo(ship_copy);
 
     cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
 
@@ -104,27 +154,28 @@ Mat detectAndMask( Mat frame )
     face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(80, 80) );
 
     if(faces.size() == 0){
-        return ship;
+        return ship_copy;
     }
 
     Mat faceROI = frame( faces[0] );
     player_x = frame.cols - faces[0].x - faces[0].width;
     wide = frame.cols - faces[0].width;
 
-    Mat mask = imread("mask.jpg",IMREAD_GRAYSCALE);
+    //Mat mask = imread("mask.jpg",IMREAD_GRAYSCALE);
+    Mat re_mask;
 
-    resize(mask,mask,Size(faceROI.cols,faceROI.rows));
+    resize(mask,re_mask,Size(faceROI.cols,faceROI.rows));
 
     vector<Mat> channels;
     split(faceROI, channels);
 
-    channels.push_back(mask);
+    channels.push_back(re_mask);
     Mat result;
     merge(channels, result);
     resize(result,result,Size(100,100));
 
     Mat player;
-    overlayImage(ship, result, player,Point(26,68));
+    overlayImage(ship_copy, result, player,Point(26,68));
 
     return player;
 
